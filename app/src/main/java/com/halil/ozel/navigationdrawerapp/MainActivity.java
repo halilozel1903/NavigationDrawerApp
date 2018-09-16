19fa6f5bb2abef26aa29c4af8b485d574c5f16ba
Halil Özel <halilozel1903@gmail.com>
2018-09-17 00:51:47 +0300

    Proje olusturuldu.

---
+++ app/src/main/java/com/halil/ozel/navigationdrawerapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.halil.ozel.navigationdrawerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar toolbar;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setUpToolbar();
+        setUpDrawer();
+    }
+
+    private void setUpToolbar(){
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("NawDrawer Örnek");
+        toolbar.setSubtitle("Örnek Uygulama");
+    }
+
+    private void setUpDrawer(){
+
+    }
+}
